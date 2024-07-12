367ab314bdacf2d3dbfefbb776cfde9ebd99af0d
trungnguyen <quangtrung01654748714@gmail.com>
2024-07-12 11:54:04 +0700

    up

--- check_and_restart.sh
+++ check_and_restart.sh
@@ -6,17 +6,29 @@ CHAT_ID="4241124330"
 MESSAGE="Docker container restarted due to error."
 
 # Hàm gửi thông báo qua Telegram
+# send_telegram_message() {
+#     curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+#         -d chat_id="${CHAT_ID}" \
+#         -d text="${MESSAGE}"
+# }
+
+# # Kiểm tra xem dịch vụ có chạy không
+# docker-compose ps | grep 'Exit' &> /dev/null
+# if [ $? -eq 0 ]; then
+#   # Nếu dịch vụ không chạy, khởi động lại và gửi thông báo
+#   docker-compose down
+#   docker-compose up -d
+#   send_telegram_message
+# fi
 send_telegram_message() {
     curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
         -d chat_id="${CHAT_ID}" \
         -d text="${MESSAGE}"
 }
 
-# Kiểm tra xem dịch vụ có chạy không
+# Kiểm tra xem container có đang chạy không
 docker-compose ps | grep 'Exit' &> /dev/null
 if [ $? -eq 0 ]; then
-  # Nếu dịch vụ không chạy, khởi động lại và gửi thông báo
-  docker-compose down
-  docker-compose up -d
+  # Gửi thông báo nếu container bị tắt
   send_telegram_message
 fi
