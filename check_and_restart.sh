32c1a9906d1f8bc493078127548ffd9bfb071f79
trungnguyen <quangtrung01654748714@gmail.com>
2024-07-12 14:07:11 +0700

    up

--- check_and_restart.sh
+++ check_and_restart.sh
@@ -1,16 +1,17 @@
-!/bin/bash
+#!/bin/bash
 
 LOGFILE="/root/server/server-nami/logfile.log"
+COMPOSE_FILE="/root/server/server-nami/docker-compose.yml"  # Add this line
 
 echo "$(date): Checking Docker services..." >> $LOGFILE
 
-if docker-compose ps | grep -q 'Up'; then   # Check for running containers
+if docker-compose -f "$COMPOSE_FILE" ps | grep -q 'Up'; then   
     echo "$(date): All services are running." >> $LOGFILE
-elif docker-compose ps | grep -q 'Exit'; then   # Check for exited containers
+elif docker-compose -f "$COMPOSE_FILE" ps | grep -q 'Exit'; then   
     echo "$(date): Some containers have exited. Restarting services..." >> $LOGFILE
-    docker-compose down >> $LOGFILE 2>&1
-    docker-compose up -d >> $LOGFILE 2>&1
+    docker-compose -f "$COMPOSE_FILE" down >> $LOGFILE 2>&1
+    docker-compose -f "$COMPOSE_FILE" up -d >> $LOGFILE 2>&1
 else
     echo "$(date): No containers running. Starting services..." >> $LOGFILE
-    docker-compose up -d >> $LOGFILE 2>&1  # Start if no containers found
+    docker-compose -f "$COMPOSE_FILE" up -d >> $LOGFILE 2>&1
 fi
