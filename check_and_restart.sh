447b741d85d2fdded6d468ae27a257fce1bff1e5
trungnguyen <quangtrung01654748714@gmail.com>
2024-07-15 13:03:08 +0700

    up

--- check_and_restart.sh
+++ check_and_restart.sh
@@ -1,43 +1,44 @@
+#!/bin/bash
+
+CONTAINER_NAME="nodeappcontainer"
+LOG_FILE="/root/server/server-nami/restart.log"
+
+# Lấy 100 dòng cuối cùng của log container
+LOGS=$(docker logs --tail 50 $CONTAINER_NAME 2>&1)
+
+# Kiểm tra xem log mới nhất có chứa lỗi "Too many connections" không
+if echo "$LOGS" | grep -q "Too many connections"; then
+  # Ghi log
+  echo "$(date) - Too many connections error detected. Restarting container $CONTAINER_NAME." >> "$LOG_FILE"
+  # Khởi động lại container
+  docker restart "$CONTAINER_NAME"
+fi
+
+
+
 # #!/bin/bash
 
 # # Tên của container Docker bạn muốn kiểm tra
 # CONTAINER_NAME="nodeappcontainer"
 
-# # Lệnh để kiểm tra log của container
-# LOGS=$(docker logs $CONTAINER_NAME 2>&1)
+# # Đường dẫn tạm thời để lưu log
+# TEMP_LOG="/root/server/server-nami/${CONTAINER_NAME}_log.tmp"
+
+# # Kiểm tra xem file tạm thời có tồn tại không
+# if [ ! -f "$TEMP_LOG" ]; then
+#   # Tạo file tạm thời nếu chưa tồn tại
+#   touch "$TEMP_LOG"
+# fi
+
+# # Lấy log mới nhất của container
+# docker logs $CONTAINER_NAME > "$TEMP_LOG" 2>&1
 
 # # Kiểm tra xem log có chứa lỗi "Too many connections" không
-# if echo "$LOGS" | grep -q "Too many connections"; then
+# if grep -q "Too many connections" "$TEMP_LOG"; then
 #   # Ghi log
 #   echo "$(date) - Too many connections error detected. Restarting container $CONTAINER_NAME." >> /root/server/server-nami/restart.log
 #   # Khởi động lại container
 #   docker restart "$CONTAINER_NAME"
+#   # Xóa log cũ của container sau khi khởi động lại
+#   docker logs --since 0m $CONTAINER_NAME > /dev/null 2>&1
 # fi
-
-
-#!/bin/bash
-
-# Tên của container Docker bạn muốn kiểm tra
-CONTAINER_NAME="nodeappcontainer"
-
-# Đường dẫn tạm thời để lưu log
-TEMP_LOG="/root/server/server-nami/${CONTAINER_NAME}_log.tmp"
-
-# Kiểm tra xem file tạm thời có tồn tại không
-if [ ! -f "$TEMP_LOG" ]; then
-  # Tạo file tạm thời nếu chưa tồn tại
-  touch "$TEMP_LOG"
-fi
-
-# Lấy log mới nhất của container
-docker logs $CONTAINER_NAME > "$TEMP_LOG" 2>&1
-
-# Kiểm tra xem log có chứa lỗi "Too many connections" không
-if grep -q "Too many connections" "$TEMP_LOG"; then
-  # Ghi log
-  echo "$(date) - Too many connections error detected. Restarting container $CONTAINER_NAME." >> /root/server/server-nami/restart.log
-  # Khởi động lại container
-  docker restart "$CONTAINER_NAME"
-  # Xóa log cũ của container sau khi khởi động lại
-  docker logs --since 0m $CONTAINER_NAME > /dev/null 2>&1
-fi
